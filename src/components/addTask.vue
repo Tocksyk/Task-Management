a10dde5f5d9a92eef8e5921001fd9fe75df60941
Sam <sameeda998@gmail.com>
2022-05-24 23:50:48 +0530

    Addtask feature added in the template

---
+++ src/components/addTask.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+<template>
+<div class="taskAddition">
+    <span>
+        <form action="submit">
+            <input type="text" name="adding">
+            <button type="submit"> ADD </button>
+        </form>
+    </span>
+</div>
+</template>
+<style></style>
